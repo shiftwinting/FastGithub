849d622bc6a624d7c4eb8243b832a7e9f7ee5bc0
陈国伟 <366193849@qq.com>
2021-06-16 13:22:42 +0800

    调整默认时间间隔

--- FastGithub.Scanner/GithubOptions.cs
+++ FastGithub.Scanner/GithubOptions.cs
@@ -5,9 +5,9 @@ namespace FastGithub.Scanner
     [Options("Github")]
     sealed class GithubOptions
     {
-        public TimeSpan ScanAllInterval { get; set; } = TimeSpan.FromHours(12d);
+        public TimeSpan ScanAllInterval { get; set; } = TimeSpan.FromHours(2d);
 
-        public TimeSpan ScanResultInterval { get; set; } = TimeSpan.FromMinutes(5d);
+        public TimeSpan ScanResultInterval { get; set; } = TimeSpan.FromMinutes(1d);
 
         public Uri MetaUri { get; set; } = new Uri("https://gitee.com/jiulang/fast-github/raw/master/FastGithub/meta.json");
 
