5b1e7d8a7023d78c6c857f16da3798e953972ea2
xljiulang <366193849@qq.com>
2021-06-20 16:02:27 +0800

    增加osx-x64发布

--- FastGithub/publish.cmd
+++ FastGithub/publish.cmd
@@ -1,3 +1,4 @@
 dotnet publish -c Release -f net6.0 /p:PublishSingleFile=true /p:PublishTrimmed=true -r win-x86 -o ./bin/publish/win-x86
 dotnet publish -c Release -f net6.0 /p:PublishSingleFile=true /p:PublishTrimmed=true -r win-x64 -o ./bin/publish/win-x64
 dotnet publish -c Release -f net6.0 /p:PublishSingleFile=true /p:PublishTrimmed=true -r linux-x64 -o ./bin/publish/linux-x64
+dotnet publish -c Release -f net6.0 /p:PublishSingleFile=true /p:PublishTrimmed=true -r osx-x64 -o ./bin/publish/osx-x64
