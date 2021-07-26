f83f86cb3cecd9e02f2ae9c9d08f7c1f8f1c3bb5
陈国伟 <366193849@qq.com>
2021-07-26 10:29:37 +0800

    只保留.net6

--- FastGithub/zip.sh
+++ FastGithub/zip.sh
@@ -2,8 +2,8 @@
 cd ./bin/publish
 
 # linux
-chmod +777 ./linux-x64/dnscrypt-proxy
-chmod +777 ./linux-x64/FastGithub
+chmod 777 ./linux-x64/dnscrypt-proxy
+chmod 777 ./linux-x64/FastGithub
 zip -r linux-x64.zip linux-x64
 
 # windows
