b02c6306e8e26a72fe66da64c53d81590aa8d613
陈国伟 <366193849@qq.com>
2021-07-26 10:17:50 +0800

    zip打包

---
+++ FastGithub/zip.sh
@@ -0,0 +1,10 @@
+#! /bin/bash  # employ bash shell
+cd ./bin/publish
+
+# linux
+chmod +777 ./linux-x64/dnscrypt-proxy
+chmod +777 ./linux-x64/FastGithub
+zip -r linux-x64.zip linux-x64
+
+# windows
+zip -r win-x64.zip win-x64 -x "./win-x64/aspnetcorev2_inprocess.dll"
