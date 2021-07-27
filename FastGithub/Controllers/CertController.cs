6891fc06bd255c3e6624343cb01f234782b1d997
陈国伟 <366193849@qq.com>
2021-07-27 17:32:52 +0800

    修改首页

---
+++ FastGithub/Controllers/CertController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace FastGithub.Controllers
+{
+    public class CertController : Controller
+    {
+        /// <summary>
+        /// 下载CA证书
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IActionResult> Index()
+        {
+            var certFile = $"CACert/{nameof(FastGithub)}.cer";
+            this.Response.ContentType = "application/x-x509-ca-cert";
+            this.Response.Headers.Add("Content-Disposition", $"attachment;filename={nameof(FastGithub)}.cer");
+            await this.Response.SendFileAsync(certFile);
+            return new EmptyResult();
+        }
+    }
+}
