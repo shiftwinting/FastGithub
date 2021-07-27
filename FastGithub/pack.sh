e270564d171a989dffaecd9a162e6eee62e527f6
陈国伟 <366193849@qq.com>
2021-07-27 10:43:34 +0800

    osx-x64

--- FastGithub/pack.sh
+++ FastGithub/pack.sh
@@ -1,10 +1,15 @@
 #! /bin/bash
 cd ./bin/publish
 
-# linux
+# linux-x64
 chmod 777 ./linux-x64/dnscrypt-proxy
 chmod 777 ./linux-x64/FastGithub
 zip -r linux-x64.zip linux-x64
 
-# windows
+# osx-x64
+chmod 777 ./osx-x64/dnscrypt-proxy
+chmod 777 ./osx-x64/FastGithub
+zip -r osx-x64.zip osx-x64
+
+# windows-x64
 zip -r win-x64.zip win-x64 -x "./win-x64/aspnetcorev2_inprocess.dll"
--- FastGithub/publish.cmd
+++ FastGithub/publish.cmd
@@ -2,3 +2,4 @@ set output=./bin/publish
 if exist "%output%" rd /S /Q "%output%"
 dotnet publish -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true -r win-x64 -o "%output%/win-x64"
 dotnet publish -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true -r linux-x64 -o "%output%/linux-x64"
+dotnet publish -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true -r osx-x64 -o "%output%/osx-x64"
