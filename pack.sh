566930208b7d8038986424dec67dfe4fc2ae3b86
xljiulang <366193849@qq.com>
2021-07-29 20:49:10 +0800

    增加.net5发布以支持win7 #16

--- pack.sh
+++ pack.sh
@@ -11,5 +11,8 @@ chmod 777 ./osx-x64/dnscrypt-proxy
 chmod 777 ./osx-x64/FastGithub
 zip -r osx-x64.zip osx-x64
 
-# windows-x64
-zip -r win-x64.zip win-x64 -x "./win-x64/aspnetcorev2_inprocess.dll"
+# win10-x64
+zip -r win10-x64.zip win10-x64 -x "./win-x64/aspnetcorev2_inprocess.dll"
+
+# win7-x64
+zip -r win7-x64.zip win7-x64
