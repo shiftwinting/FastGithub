a5714bc512a9ab9282007d1e9dc2874677839f88
陈国伟 <366193849@qq.com>
2021-08-18 16:36:11 +0800

    win7裁剪

--- publish.cmd
+++ publish.cmd
@@ -2,6 +2,6 @@ cd ./FastGithub
 set output=./bin/publish
 if exist "%output%" rd /S /Q "%output%"
 dotnet publish -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true -f net6.0 -r win-x64 -o "%output%/win10-x64"
-dotnet publish -c Release /p:PublishSingleFile=true /p:PublishTrimmed=false -f net5.0 -r win-x64 -o "%output%/win7-x64"
+dotnet publish -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true -f net5.0 -r win-x64 -o "%output%/win7-x64"
 dotnet publish -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true -f net6.0 -r linux-x64 -o "%output%/linux-x64"
 dotnet publish -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true -f net6.0 -r osx-x64 -o "%output%/osx-x64"
