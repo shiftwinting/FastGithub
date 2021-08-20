b3986ed081115ad276679a39a90775daa8580114
陈国伟 <366193849@qq.com>
2021-08-20 09:13:15 +0800

    --self-contained

--- publish.cmd
+++ publish.cmd
@@ -1,7 +1,7 @@
 cd ./FastGithub
 set output=./bin/publish
 if exist "%output%" rd /S /Q "%output%"
-dotnet publish -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true -f net6.0 -r win-x64 -o "%output%/win10-x64"
-dotnet publish -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true -f net5.0 -r win-x64 -o "%output%/win7-x64"
-dotnet publish -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true -f net6.0 -r linux-x64 -o "%output%/linux-x64"
-dotnet publish -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true -f net6.0 -r osx-x64 -o "%output%/osx-x64"
+dotnet publish -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true -f net6.0 --self-contained -r win-x64 -o "%output%/FastGithub_win10-x64"
+dotnet publish -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true -f net5.0 --self-contained -r win-x64 -o "%output%/FastGithub_win7-x64"
+dotnet publish -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true -f net6.0 --self-contained -r linux-x64 -o "%output%/FastGithub_linux-x64"
+dotnet publish -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true -f net6.0 --self-contained -r osx-x64 -o "%output%/FastGithub_osx-x64"
