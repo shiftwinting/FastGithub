95bb52b7f8201b1ebbc264b82a097ff5bc74b304
陈国伟 <366193849@qq.com>
2021-09-15 10:49:40 +0800

    打包-x

--- pack.sh
+++ pack.sh
@@ -4,15 +4,15 @@ cd ./FastGithub/bin/publish
 # linux-x64
 chmod 777 ./FastGithub_linux-x64/FastGithub
 chmod 777 ./FastGithub_linux-x64/dnscryptproxy/dnscrypt-proxy
-zip -r FastGithub_linux-x64.zip FastGithub_linux-x64
+zip -r FastGithub_linux-x64.zip FastGithub_linux-x64 -x "./FastGithub_linux-x64/x64/*" -x "./FastGithub_linux-x64/x86/*" -x "./FastGithub_linux-x64/*.pdb"
 
 # osx-x64
 chmod 777 ./FastGithub_osx-x64/FastGithub
 chmod 777 ./FastGithub_osx-x64/dnscryptproxy/dnscrypt-proxy
-zip -r FastGithub_osx-x64.zip FastGithub_osx-x64
+zip -r FastGithub_osx-x64.zip FastGithub_osx-x64 -x "./FastGithub_osx-x64/x64/*" -x "./FastGithub_osx-x64/x86/*" -x "./FastGithub_osx-x64/*.pdb"
 
 # win10-x64
-zip -r FastGithub_win10-x64.zip FastGithub_win10-x64 -x "./FastGithub_win10-x64/aspnetcorev2_inprocess.dll"
+zip -r FastGithub_win10-x64.zip FastGithub_win10-x64 -x "./FastGithub_win10-x64/x86/*" -x "./FastGithub_win10-x64/*.pdb" -x "./FastGithub_win10-x64/aspnetcorev2_inprocess.dll"
 
 # win7-x64
-zip -r FastGithub_win7-x64.zip FastGithub_win7-x64
+zip -r FastGithub_win7-x64.zip FastGithub_win7-x64 -x "./FastGithub_win7-x64/x86/*" -x "./FastGithub_win7-x64/*.pdb"
