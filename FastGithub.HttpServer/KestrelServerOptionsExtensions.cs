e00c6c14cdf8c12b53bf099c66592fb2f1debba3
陈国伟 <366193849@qq.com>
2021-09-18 10:45:11 +0800

    取消dataRate限制

--- FastGithub.HttpServer/KestrelServerOptionsExtensions.cs
+++ FastGithub.HttpServer/KestrelServerOptionsExtensions.cs
@@ -23,6 +23,8 @@ namespace FastGithub
         public static void NoLimit(this KestrelServerOptions kestrel)
         {
             kestrel.Limits.MaxRequestBodySize = null;
+            kestrel.Limits.MinResponseDataRate = null;
+            kestrel.Limits.MinRequestBodyDataRate = null;
         }
 
         /// <summary>
