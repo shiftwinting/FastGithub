1ecb0f81f8ed8b2fba44f73045f71c5f96d7df3c
老九 <366193849@qq.com>
2021-09-20 13:44:58 +0800

    修改dnscrypt-proxy目录

--- pack.sh
+++ pack.sh
@@ -6,10 +6,10 @@ zip -r fastgithub_win-x64.zip fastgithub_win-x64 -x "./fastgithub_win-x64/x86/*"
 
 # linux-x64
 chmod 777 ./fastgithub_linux-x64/fastgithub
-chmod 777 ./fastgithub_linux-x64/dnscryptproxy/dnscrypt-proxy
+chmod 777 ./fastgithub_linux-x64/dnscrypt-proxy/dnscrypt-proxy
 zip -r fastgithub_linux-x64.zip fastgithub_linux-x64 -x "./fastgithub_linux-x64/x64/*" -x "./fastgithub_linux-x64/x86/*" -x "./fastgithub_linux-x64/*.pdb"
 
 # osx-x64
 chmod 777 ./fastgithub_osx-x64/fastgithub
-chmod 777 ./fastgithub_osx-x64/dnscryptproxy/dnscrypt-proxy
+chmod 777 ./fastgithub_osx-x64/dnscrypt-proxy/dnscrypt-proxy
 zip -r fastgithub_osx-x64.zip fastgithub_osx-x64 -x "./fastgithub_osx-x64/x64/*" -x "./fastgithub_osx-x64/x86/*" -x "./fastgithub_osx-x64/*.pdb"
