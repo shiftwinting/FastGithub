5a1180d19860a129fa2f536ada889432063bd900
陈国伟 <366193849@qq.com>
2021-11-05 13:28:13 +0800

    增加linux-arm64

--- pack.sh
+++ pack.sh
@@ -9,6 +9,11 @@ chmod 777 ./fastgithub_linux-x64/fastgithub
 chmod 777 ./fastgithub_linux-x64/dnscrypt-proxy/dnscrypt-proxy
 zip -r fastgithub_linux-x64.zip fastgithub_linux-x64
 
+# linux-arm64
+chmod 777 ./fastgithub_linux-arm64/fastgithub
+chmod 777 ./fastgithub_linux-arm64/dnscrypt-proxy/dnscrypt-proxy
+zip -r fastgithub_linux-arm64.zip fastgithub_linux-arm64
+
 # osx-x64
 chmod 777 ./fastgithub_osx-x64/fastgithub
 chmod 777 ./fastgithub_osx-x64/dnscrypt-proxy/dnscrypt-proxy
