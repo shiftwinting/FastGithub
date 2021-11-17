516f04a7d781356769252c16d13de6372118b76e
陈国伟 <366193849@qq.com>
2021-11-17 10:33:59 +0800

    取消仅使用配置的IP

--- FastGithub.Http/HttpClientHandler.cs
+++ FastGithub.Http/HttpClientHandler.cs
@@ -175,13 +175,17 @@ namespace FastGithub.Http
         /// <returns></returns>
         private async IAsyncEnumerable<IPEndPoint> GetIPEndPointsAsync(DnsEndPoint dnsEndPoint, [EnumeratorCancellation] CancellationToken cancellationToken)
         {
-            if (IPAddress.TryParse(this.domainConfig.IPAddress, out var address) ||
-                IPAddress.TryParse(dnsEndPoint.Host, out address))
+            if (IPAddress.TryParse(dnsEndPoint.Host, out var address))
             {
                 yield return new IPEndPoint(address, dnsEndPoint.Port);
             }
             else
             {
+                if(IPAddress.TryParse(this.domainConfig.IPAddress, out address))
+                {
+                    yield return new IPEndPoint(address, dnsEndPoint.Port);
+                }
+                 
                 await foreach (var item in this.domainResolver.ResolveAllAsync(dnsEndPoint, cancellationToken))
                 {
                     yield return new IPEndPoint(item, dnsEndPoint.Port);
